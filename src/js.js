e2ecc796365b85aa99e751db09c89563f652af28
Yes7933 <162532903+Yes7933@users.noreply.github.com>
2025-05-15 18:59:48 +0000

    Co-authored-by: pshen5282 <pshen5282@users.noreply.github.com>

--- src/js.js
+++ src/js.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let cooldown = 2000;
+    let cooldown = 150;
 
     let combo = 0;
     let time = 0;
@@ -14,19 +14,38 @@ document.addEventListener("DOMContentLoaded", () => {
     let comboZoneWidth = 20;
 
     let fps1, fps;
+    let comboTextElement = document.getElementById("comboText")
+
+    let comboFlavorText = [
+        "COMBO LOST",
+        "Good",
+        "Great",
+        "Amazing",
+        "SUPERB!",
+        "SPECTACULAR!!",
+        "GODLIKE!!!"
+    ];
+
     document.addEventListener("keydown", (e) => {
         if (!spaceDown && e.key === " " && cooldown <= cooldownTimer) {
             spaceDown = true;
             let lineTop = document.getElementById("line").getBoundingClientRect().top + document.getElementById("line").getBoundingClientRect().height / 2;
             let barTop = document.getElementById("innerMinigameBar").getBoundingClientRect().top;
             let barBottom = document.getElementById("innerMinigameBar").getBoundingClientRect().bottom;
-            progress += (1 - Math.abs(lineTop / (window.innerHeight * 1) - 0.5) * 2) * progressGain * (1 + (comboMulti * combo));
-            progress = Math.min(1, progress);
             if (lineTop <= barBottom && lineTop >= barTop) {
                 combo++;
+                shake(20 * (combo / 2),5 * combo,10,0.95)
+                comboTextElement.innerHTML = "Combo X"+combo+" "+comboFlavorText[((combo > 6) ? 6 : combo)]
             } else {
+                if (combo > 1) {
+                    // show combo has beel lost
+                }
                 combo = 0;
+                comboTextElement.innerHTML = "Combo X"+combo+" "+comboFlavorText[((combo > 6) ? 6 : combo)]
+                shake(8,5,10,1)
             }
+            progress += (1 - Math.abs(lineTop / (window.innerHeight * 1) - 0.5) * 2) * progressGain * (1 + (comboMulti * combo));
+            progress = Math.min(1, progress);
             cooldownTimer = 0;
         }
     });
@@ -37,8 +56,8 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     function startMinigame(cSM, pL, pG, cZW) {
         comboSpeedMultiplier = cSM
-        progressLost = pL
-        progressGain = pG
+        progressLost = pL / 100000
+        progressGain = pG / 5
         comboZoneWidth = cZW
         combo = 0,
         time = 0;
@@ -47,8 +66,22 @@ document.addEventListener("DOMContentLoaded", () => {
         fps1 = Date.now();
         window.requestAnimationFrame(minigameLoop);
     }
+    function shake(magnitude, repeat, interval, decay) {
+        let elemen = document.getElementById("minigameUI")
+        if (repeat == 0) {
+            elemen.style.left = '10%'
+            elemen.style.top = '30%'
+        }
+        else {
+            let r1 = (Math.random() - 0.5) * magnitude
+            let r2 = (Math.random() - 0.5) * magnitude
+            elemen.style.left = 'calc(10% + '+r1+'px)'
+            elemen.style.top = 'calc(30% + '+r2+'px)'
+            setTimeout(shake, interval, magnitude * decay, repeat - 1, interval, decay)
+        }
+    }
     function minigameLoop() {
-        document.getElementById("line").style.top = (document.getElementById("minigameBar").getBoundingClientRect().height/2/window.innerHeight * 100 + document.getElementById("minigameBar").getBoundingClientRect().height/2/window.innerHeight * 100 * (Math.atan(2 * Math.sin(time / 100)) / Math.atan(2))) + "%";
+        document.getElementById("line").style.top = (document.getElementById("minigameBar").getBoundingClientRect().height/window.innerHeight * 125 + document.getElementById("minigameBar").getBoundingClientRect().height/window.innerHeight * 125 * (Math.atan(2 * Math.sin(time / 100)) / Math.atan(2))) + "%";
         let fps2 = Date.now()
         fps = fps2 - fps1;
         time += fps * (comboSpeedMultiplier * (1 + combo));
@@ -68,5 +101,5 @@ document.addEventListener("DOMContentLoaded", () => {
         */
        window.requestAnimationFrame(minigameLoop); 
     }
-    startMinigame(0.1,-1/100000,1/5,25)
+    startMinigame(0.1,-5,0.75,150)
 });
