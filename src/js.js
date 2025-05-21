937a00367cf8411ba9388f23fd40536089552f7b
Yes7933 <162532903+Yes7933@users.noreply.github.com>
2025-05-21 18:41:04 +0000

    Co-authored-by: pshen5282 <pshen5282@users.noreply.github.com>

--- src/js.js
+++ src/js.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let cooldown = 150;
+    let inventory = {
+        "Catfish": 23,
+        "Pike": 17,
+        "Anchovy": 5,
+        "Halibut": 2,
+        "Bass": 3,
+    }
+    let menu = null;
+    let cooldown = 100;
 
     let combo = 0;
     let time = 0;
     let progress = 0;
-    let comboMulti = 1;
+    let comboMulti = 0.25;
     let spaceDown = false;
     let cooldownTimer = 0;
 
@@ -14,16 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let comboZoneWidth = 20;
 
     let fps1, fps;
-    let comboTextElement = document.getElementById("comboText")
+    let comboTextElement = document.getElementById("comboText");
 
     let comboFlavorText = [
-        "COMBO LOST",
-        "Good",
-        "Great",
-        "Amazing",
-        "SUPERB!",
-        "SPECTACULAR!!",
-        "GODLIKE!!!"
+        ["Combo Broken...", "Combo Lost..."],
+        ["Good!", "Nice!"],
+        ["Great!", "Cool!"],
+        ["Amazing!", "Wow!"],
+        ["SUPERB!", "CRAZY!"],
+        ["SPECTACULAR!!", "UNBELIEVEABLE!!"],
+        ["GODLIKE!!!", "IMPOSSIBLE!!!"],
     ];
 
     document.addEventListener("keydown", (e) => {
@@ -34,72 +42,109 @@ document.addEventListener("DOMContentLoaded", () => {
             let barBottom = document.getElementById("innerMinigameBar").getBoundingClientRect().bottom;
             if (lineTop <= barBottom && lineTop >= barTop) {
                 combo++;
-                shake(20 * (combo / 2),5 * combo,10,0.95)
-                comboTextElement.innerHTML = "Combo X"+combo+" "+comboFlavorText[((combo > 6) ? 6 : combo)]
+                shake(20 * (combo / 2), 5 * combo, 10, 0.95);
+                comboTextElement.style.transitionDuration = "0ms"
+                comboTextElement.style.rotate = combo*360 + "deg"
+                comboTextElement.style.transitionDuration = "500ms"
+                setTimeout(() => {
+                    comboTextElement.style.transitionDuration = "0ms"
+                    comboTextElement.style.rotate = "0deg";
+                }, 500)
             } else {
-                if (combo > 1) {
-                    // show combo has beel lost
-                }
                 combo = 0;
-                comboTextElement.innerHTML = "Combo X"+combo+" "+comboFlavorText[((combo > 6) ? 6 : combo)]
-                shake(8,5,10,1)
+                shake(8, 5, 10, 1);
             }
-            progress += (1 - Math.abs(lineTop / (window.innerHeight * 1) - 0.5) * 2) * progressGain * (1 + (comboMulti * combo));
+            let tempList = comboFlavorText[Math.min(combo, 6)];
+            comboTextElement.textContent = tempList[Math.floor(Math.random() * tempList.length)] + " x" + combo;
+            comboTextElement.style.fontSize = (combo + 2) + "vmin";
+            progress += (1 - Math.abs(lineTop / (window.innerHeight * 1) - 0.5) * 2) * progressGain * (1 + comboMulti * Math.min(6, combo));
             progress = Math.min(1, progress);
             cooldownTimer = 0;
+        } else {
         }
     });
     document.addEventListener("keyup", (e) => {
         if (spaceDown && e.key === " ") {
             spaceDown = false;
         }
-    })
+    });
+    /**
+     * Triggers the fishing minigame with settings.
+     * @param {Number} cSM - Mulitplier when a combo is reached. 
+     * @param {Number} pL - Progress lost factor (divided by 100000).
+     * @param {Number} pG - Progress gain factor (divided by 5).
+     * @param {Number} cZW - Width for the zone where a combo is achieved (0, 70]
+     */
     function startMinigame(cSM, pL, pG, cZW) {
-        comboSpeedMultiplier = cSM
-        progressLost = pL / 100000
-        progressGain = pG / 5
-        comboZoneWidth = cZW
-        combo = 0,
-        time = 0;
-        progress = 0,
-        document.getElementById("innerMinigameBar").style.height = (comboZoneWidth + "%")
+        comboSpeedMultiplier = cSM;
+        progressLost = (-1 * pL) / 100000;
+        progressGain = pG / 5;
+        comboZoneWidth = cZW;
+        (combo = 0), (time = 0);
+        progress = 0;
+        let gradientSplit = (100 - cZW) / 6;
+        document.getElementById("innerMinigameBar").style.height = comboZoneWidth + "%";
+        document.getElementById("minigameBar").style.background = `linear-gradient(rgba(255, 0, 0, 1) 0%, rgba(255, 0, 0, 1) ${gradientSplit - 5}%, rgba(255, 127, 0, 1) ${gradientSplit + 5}%, rgba(255, 127, 0, 1) ${gradientSplit * 2 - 5}%, rgba(255, 255, 0, 1) ${gradientSplit * 2 + 5}%, rgba(255, 255, 0, 1) ${gradientSplit * 3}%, rgba(0, 255, 0, 1) ${
+            gradientSplit * 3 + 2
+        }%, rgba(0, 255, 0, 1) ${gradientSplit * 3 + cZW - 2}%, rgba(255, 255, 0, 1) ${gradientSplit * 3 + cZW}%, rgba(255, 255, 0, 1) ${gradientSplit * 4 + cZW - 5}%, rgba(255, 127, 0, 1) ${gradientSplit * 4 + cZW + 5}%, rgba(255, 127, 0, 1) ${gradientSplit * 5 + cZW - 5}%, rgba(255, 0, 0, 1) ${gradientSplit * 5 + cZW + 5}%,  rgba(255, 0, 0, 1) 100%)`;
         fps1 = Date.now();
         window.requestAnimationFrame(minigameLoop);
     }
     function shake(magnitude, repeat, interval, decay) {
-        let elemen = document.getElementById("minigameUI")
+        let elemen = document.getElementById("minigameUI");
         if (repeat == 0) {
-            elemen.style.left = '10%'
-            elemen.style.top = '30%'
-        }
-        else {
-            let r1 = (Math.random() - 0.5) * magnitude
-            let r2 = (Math.random() - 0.5) * magnitude
-            elemen.style.left = 'calc(10% + '+r1+'px)'
-            elemen.style.top = 'calc(30% + '+r2+'px)'
-            setTimeout(shake, interval, magnitude * decay, repeat - 1, interval, decay)
+            elemen.style.left = "10%";
+            elemen.style.top = "30%";
+        } else {
+            let r1 = (Math.random() - 0.5) * magnitude;
+            let r2 = (Math.random() - 0.5) * magnitude;
+            elemen.style.left = "calc(10% + " + r1 + "px)";
+            elemen.style.top = "calc(30% + " + r2 + "px)";
+            setTimeout(shake, interval, magnitude * decay, repeat - 1, interval, decay);
         }
     }
     function minigameLoop() {
-        document.getElementById("line").style.top = (document.getElementById("minigameBar").getBoundingClientRect().height/window.innerHeight * 125 + document.getElementById("minigameBar").getBoundingClientRect().height/window.innerHeight * 125 * (Math.atan(2 * Math.sin(time / 100)) / Math.atan(2))) + "%";
-        let fps2 = Date.now()
-        fps = fps2 - fps1;
-        time += fps * (comboSpeedMultiplier * (1 + combo));
-        progress += fps * progressLost;
-        cooldownTimer += fps;
-        progress = Math.max(0, progress);
-        document.getElementById("line").style.backgroundColor = (cooldown <= cooldownTimer) ? "white" : "black";
-        document.getElementById("innerProgressBar").style.height = progress * 100 + "%";
-        fps2 = fps1;
-        fps1 = Date.now();
-        /*
+            document.getElementById("line").style.top = (document.getElementById("minigameBar").getBoundingClientRect().height / window.innerHeight) * 125 + (document.getElementById("minigameBar").getBoundingClientRect().height / window.innerHeight) * 115 * (Math.atan(2 * Math.sin(time / 100)) / Math.atan(2)) + "%";
+            let fps2 = Date.now();
+            fps = fps2 - fps1;
+            time += fps * (comboSpeedMultiplier * (1 + combo));
+            progress += fps * progressLost;
+            cooldownTimer += fps;
+            progress = Math.max(0, progress);
+            document.getElementById("innerProgressBar").style.height = progress * 100 + "%";
+            document.getElementById("line").style.backgroundColor = `rgb(${255 * Math.min(cooldownTimer / cooldown, 1)}, ${255 * Math.min(cooldownTimer / cooldown, 1)}, ${255 * Math.min(cooldownTimer / cooldown, 1)})`;
+            document.getElementById("innerProgressBarTwo").style.opacity = Math.min(cooldownTimer / cooldown, 1);
+            let lineTop = document.getElementById("line").getBoundingClientRect().top + document.getElementById("line").getBoundingClientRect().height / 2;
+            let barTop = document.getElementById("innerMinigameBar").getBoundingClientRect().top;
+            let barBottom = document.getElementById("innerMinigameBar").getBoundingClientRect().bottom;
+            document.getElementById("innerProgressBarTwo").style.height = (1 - Math.abs(lineTop / (window.innerHeight * 1) - 0.5) * 2) * progressGain * (1 + comboMulti * Math.min(6, combo)) * 100 + "%";
+            if (lineTop <= barBottom && lineTop >= barTop) {
+                 document.getElementById("innerProgressBarTwo").style.backgroundColor = "rgb(255, 150, 192)";
+            } else {
+                document.getElementById("innerProgressBarTwo").style.backgroundColor = "rgb(150, 150, 192)";
+            }
+             fps2 = fps1;
+            fps1 = Date.now();
+            /*
         if (fps >= 166.7) {
             window.requestAnimationFrame(minigameLoop);
         } else {
             setTimeout(minigameLoop, 167);
         }
         */
-       window.requestAnimationFrame(minigameLoop); 
+            window.requestAnimationFrame(minigameLoop);
     }
-    startMinigame(0.1,-5,0.75,150)
+    startMinigame(0.1, 6, 0.5, 10);
+    document.querySelectorAll(".majorMenuButtons").forEach((e) => {
+        e.addEventListener("click", () => {
+            menu = e.id.slice(0, e.id.length - 4);
+            if (menu == "inventory") {
+                for (item in inventory) {
+                    let newElement = document.createElement("div");
+                    document.getElementById("menu").appendChild(newElement);
+                    newElement.innerHTML = `<img><div>${item}</div><div>${inventory[item]}</div>`;
+                }
+            }
+        });
+    });
 });
